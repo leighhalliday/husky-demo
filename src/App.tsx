91239a75b646fb3c7a0e708baf5328150d5aa359
Leigh Halliday <leighhalliday@gmail.com>
2021-04-05 14:07:09 -0400

    Adding lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Husky
         </a>
       </header>
     </div>
